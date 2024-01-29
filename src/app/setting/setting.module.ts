ffa31d33d42fafca6746bfe953f5764c88a2aa0f
raihan-brain <raihanuzzaman@bs-23.net>
2024-01-30 00:07:58 +0600

    service provided

--- src/app/setting/setting.module.ts
+++ src/app/setting/setting.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SettingRoutingModule } from './setting-routing.module';
+import {DarkService} from "../dashboard/services/dark.service";
 
 
 @NgModule({
   declarations: [],
+  providers: [DarkService],
   imports: [
     CommonModule,
     SettingRoutingModule
--- src/app/setting/setting/setting.component.ts
+++ src/app/setting/setting/setting.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {LandService} from "../../dashboard/services/land.service";
+import {DarkService} from "../../dashboard/services/dark.service";
 
 @Component({
   selector: 'app-setting',
@@ -10,11 +11,19 @@ import {LandService} from "../../dashboard/services/land.service";
 })
 export class SettingComponent implements OnInit{
   msgFromService: string = "No message yet";
-  constructor(private landService: LandService) { }
+  msgFromDarkService: string = "No message yet";
+  constructor(private landService: LandService, private darkService: DarkService) { }
 
   ngOnInit(): void {
     this.landService.getLands();
     this.msgFromService = this.landService.getMessageFromLandService();
+    // did it create 2 instances of darkserivce ?
+
+    // Yes, it will create two instances of `DarkService` - one for the `DashboardModule` and one for the `SettingModule`.
+    // This is because the service is provided in the `providers` array of both modules. Each Angular module has its own injector,
+    // and when a service is provided in a module, a new instance of the service is created for that module.
+    // Therefore, the `DashboardModule` and the `SettingModule` will each have their own instance of `DarkService`.
+    this.msgFromDarkService = this.darkService.sendDarkMsg();
   }
 
 
